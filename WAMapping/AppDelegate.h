eca59d6cc905a0e016494ada7894683956ed1bcc
Marian PAUL <marian.paul2@gmail.com>
2016-01-27 13:36:35 -0500

    Initial Commit

---
+++ WAMapping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WAMapping
+//
+//  Created by Marian Paul on 27/01/2016.
+//  Copyright © 2016 Wasappli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WAMapping/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WAMapping
+//
+//  Created by Marian Paul on 27/01/2016.
+//  Copyright © 2016 Wasappli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
